f86ff50ee9c91f6aeb159f3e42f377b64aac769c
Tsunekawa Yutaro <tsunekawa@ueda.info.waseda.ac.jp>
2018-09-05 20:13:05 +0900

    quick 8ac

--- quick/sample.cpp
+++ quick/sample.cpp
@@ -37,15 +37,61 @@ void quicksort_(vector<int> &a, int l, int r) {
 void quicksort(vector<int> &a) {
   int len = a.size()-1;
 
-  thread t1(quicksort_, ref(a), 0, len/4);
-  thread t2(quicksort_, ref(a), len/4+1, (len*2)/4);
-  thread t3(quicksort_, ref(a), (len*2)/4+1, (len*3)/4);
-  thread t4(quicksort_, ref(a), (len*3)/4+1, len);
+  // thread t1(quicksort_, ref(a), 0, len/4);
+  // thread t2(quicksort_, ref(a), len/4+1, (len*2)/4);
+  // thread t3(quicksort_, ref(a), (len*2)/4+1, (len*3)/4);
+  // thread t4(quicksort_, ref(a), (len*3)/4+1, len);
+
+  // t1.join();
+  // t2.join();
+  // t3.join();
+  // t4.join();
+  
+  // thread s1(inplace_merge<vector<int>::iterator>, a.begin(), a.begin()+len/4+1, a.begin()+len/2+1);
+  // thread s2(inplace_merge<vector<int>::iterator>, a.begin()+len/2+1, a.begin()+(len*3)/4+1, a.end());
+  // s1.join();
+  // s2.join();
+  
+  // // inplace_merge(a.begin(), a.begin()+len/4+1, a.begin()+len/2+1);
+  // // inplace_merge(a.begin()+len/2+1, a.begin()+(len*3)/4+1, a.end());
+  // inplace_merge(a.begin(), a.begin()+len/2+1, a.end());
+
+
+  thread t0(quicksort_, ref(a), 0, len/8);
+  thread t1(quicksort_, ref(a), len/8+1, (len*2)/8);
+  thread t2(quicksort_, ref(a), (len*2)/8+1, (len*3)/8);
+  thread t3(quicksort_, ref(a), (len*3)/8+1, (len*4)/8);
+  thread t4(quicksort_, ref(a), (len*4)/8+1, (len*5)/8);
+  thread t5(quicksort_, ref(a), (len*5)/8+1, (len*6)/8);
+  thread t6(quicksort_, ref(a), (len*6)/8+1, (len*7)/8);
+  thread t7(quicksort_, ref(a), (len*7)/8+1, len);
+  t0.join();
   t1.join();
-  t2.join();
+  t2.join();    
   t3.join();
   t4.join();
-  inplace_merge(a.begin(), a.begin()+len/4+1, a.begin()+len/2+1);
-  inplace_merge(a.begin()+len/2+1, a.begin()+(len*3)/4+1, a.end());
+  t5.join();
+  t6.join();
+  t7.join();
+  thread s0(inplace_merge<vector<int>::iterator>, a.begin(), a.begin()+len/8+1, a.begin()+len*2/8+1);
+  thread s1(inplace_merge<vector<int>::iterator>, a.begin()+len*2/8+1, a.begin()+(len*3)/8+1, a.begin()+len*4/8+1);
+  thread s2(inplace_merge<vector<int>::iterator>, a.begin()+len*4/8+1, a.begin()+(len*5)/8+1, a.begin()+len*6/8+1);
+  thread s3(inplace_merge<vector<int>::iterator>, a.begin()+len*6/8+1, a.begin()+(len*7)/8+1, a.end());
+  s0.join();
+  s1.join();
+  s2.join();
+  s3.join();
+  thread s4(inplace_merge<vector<int>::iterator>, a.begin(), a.begin()+len*2/8+1, a.begin()+len*4/8+1);
+  thread s5(inplace_merge<vector<int>::iterator>, a.begin()+len*4/8+1, a.begin()+len*6/8+1, a.end());
+  s4.join();
+  s5.join();
   inplace_merge(a.begin(), a.begin()+len/2+1, a.end());
+
+  // inplace_merge(a.begin(), a.begin()+len/8+1, a.begin()+len*2/8+1);
+  // inplace_merge(a.begin()+len*2/8+1, a.begin()+(len*3)/8+1, a.begin()+len*4/8+1);
+  // inplace_merge(a.begin()+len*4/8+1, a.begin()+(len*5)/8+1, a.begin()+len*6/8+1);
+  // inplace_merge(a.begin()+len*6/8+1, a.begin()+(len*7)/8+1, a.end());
+  // inplace_merge(a.begin(), a.begin()+len*2/8+1, a.begin()+len*4/8+1);
+  // inplace_merge(a.begin()+len*4/8+1, a.begin()+len*6/8+1, a.end());
+  // inplace_merge(a.begin(), a.begin()+len/2+1, a.end());
 }
