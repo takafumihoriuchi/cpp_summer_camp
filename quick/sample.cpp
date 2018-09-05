b53138ee5c0e8ccdad5f4ab0b9c130ae5e8305e5
Tsunekawa Yutaro <tsunekawa@ueda.info.waseda.ac.jp>
2018-09-05 16:58:17 +0900

    quick 4ac

--- quick/sample.cpp
+++ quick/sample.cpp
@@ -5,6 +5,12 @@
 #include "clock.h"
 
 using namespace std;
+void print_vec(vector<int> &a) {
+  for(auto i = a.begin(); i!=a.end(); i++) {
+    cout<< *i << " ";
+  }
+  cout<<endl;  
+}
 
 void quicksort_(vector<int> &a, int l, int r) {
   if (l < r) {
@@ -29,9 +35,17 @@ void quicksort_(vector<int> &a, int l, int r) {
   }
 }
 void quicksort(vector<int> &a) {
-  thread t1(quicksort_, ref(a), 0, (a.size()-1)/2);
-  thread t2(quicksort_, ref(a), (a.size()-1)/2+1, a.size()-1);
+  int len = a.size()-1;
+
+  thread t1(quicksort_, ref(a), 0, len/4);
+  thread t2(quicksort_, ref(a), len/4+1, (len*2)/4);
+  thread t3(quicksort_, ref(a), (len*2)/4+1, (len*3)/4);
+  thread t4(quicksort_, ref(a), (len*3)/4+1, len);
   t1.join();
   t2.join();
-  inplace_merge(a.begin(), a.begin()+(a.size()-1)/2+1, a.end());
+  t3.join();
+  t4.join();
+  inplace_merge(a.begin(), a.begin()+len/4+1, a.begin()+len/2+1);
+  inplace_merge(a.begin()+len/2+1, a.begin()+(len*3)/4+1, a.end());
+  inplace_merge(a.begin(), a.begin()+len/2+1, a.end());
 }
