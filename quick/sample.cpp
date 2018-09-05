8f4fca27120fdf6f6d1fa6869dc552a65bd45d2a
Tsunekawa Yutaro <tsunekawa@ueda.info.waseda.ac.jp>
2018-09-05 20:39:11 +0900

    quick pivot

--- quick/sample.cpp
+++ quick/sample.cpp
@@ -12,50 +12,35 @@ void print_vec(vector<int> &a) {
   cout<<endl;  
 }
 
+int pivot_sel(int x, int y, int z) {
+  if (x<y) {
+    if(y<z) return y;
+    else if(z<x) return x;
+    else return z;
+  } else {
+    if(z<y) return y;
+    else if(x<z) return x;
+    else return z;
+  }
+}
+
 void quicksort_(vector<int> &a, int l, int r) {
   if (l < r) {
-    int p = a[(l+r)/2];
+    // int p = a[(l+r)/2];
+    int p = pivot_sel(a[l], a[(l+r)/2], a[r]);
     int i = l-1, j = r+1;
     while (1) {
-      // thread t([&]{while (a[++i] < p);});
-      // thread s([&]{while (a[--j] > p);});
       while (a[++i] < p);
       while (a[--j] > p);
-      // t.join();
-      // s.join();
       if (i >= j) break;
       swap(a[i], a[j]);
     }
-    // thread t1(quicksort, ref(a), l, i-1);
-    // thread t2(quicksort, ref(a), j+1, r);
-    // t1.join();
-    // t2.join();
     quicksort_(a, l, i-1);
     quicksort_(a, j+1, r);
   }
 }
 void quicksort(vector<int> &a) {
   int len = a.size()-1;
-
-  // thread t1(quicksort_, ref(a), 0, len/4);
-  // thread t2(quicksort_, ref(a), len/4+1, (len*2)/4);
-  // thread t3(quicksort_, ref(a), (len*2)/4+1, (len*3)/4);
-  // thread t4(quicksort_, ref(a), (len*3)/4+1, len);
-
-  // t1.join();
-  // t2.join();
-  // t3.join();
-  // t4.join();
-  
-  // thread s1(inplace_merge<vector<int>::iterator>, a.begin(), a.begin()+len/4+1, a.begin()+len/2+1);
-  // thread s2(inplace_merge<vector<int>::iterator>, a.begin()+len/2+1, a.begin()+(len*3)/4+1, a.end());
-  // s1.join();
-  // s2.join();
-  
-  // // inplace_merge(a.begin(), a.begin()+len/4+1, a.begin()+len/2+1);
-  // // inplace_merge(a.begin()+len/2+1, a.begin()+(len*3)/4+1, a.end());
-  // inplace_merge(a.begin(), a.begin()+len/2+1, a.end());
-
 
   thread t0(quicksort_, ref(a), 0, len/8);
   thread t1(quicksort_, ref(a), len/8+1, (len*2)/8);
@@ -86,12 +71,4 @@ void quicksort(vector<int> &a) {
   s4.join();
   s5.join();
   inplace_merge(a.begin(), a.begin()+len/2+1, a.end());
-
-  // inplace_merge(a.begin(), a.begin()+len/8+1, a.begin()+len*2/8+1);
-  // inplace_merge(a.begin()+len*2/8+1, a.begin()+(len*3)/8+1, a.begin()+len*4/8+1);
-  // inplace_merge(a.begin()+len*4/8+1, a.begin()+(len*5)/8+1, a.begin()+len*6/8+1);
-  // inplace_merge(a.begin()+len*6/8+1, a.begin()+(len*7)/8+1, a.end());
-  // inplace_merge(a.begin(), a.begin()+len*2/8+1, a.begin()+len*4/8+1);
-  // inplace_merge(a.begin()+len*4/8+1, a.begin()+len*6/8+1, a.end());
-  // inplace_merge(a.begin(), a.begin()+len/2+1, a.end());
 }
